0c801162a63b474215d027ef05f3deea98f2f88c
Daichi Kimura <daichi703n@gmail.com>
2019-08-28 01:29:53 +0900

    fix database

--- src/main/resources/db/migration/V1_0_0__baseline.sql
+++ src/main/resources/db/migration/V1_0_0__baseline.sql
@@ -1,8 +1,10 @@
 CREATE TABLE instances (
-  id varchar(40) NOT NULL,
-  name varchar(255) NOT NULL,
-  deployment varchar(255) DEFAULT '-',
-  requireSpot boolean,
+  id VARCHAR(40) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  deployment VARCHAR(255) DEFAULT '-',
+  requireSpot BOOLEAN,
+  created_at TIMESTAMP DEFAULT NOW(),
+  updated_at TIMESTAMP DEFAULT NOW() ON UPDATE NOW(),
 
   PRIMARY KEY(deployment,name)
 );
