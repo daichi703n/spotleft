8cf60c3e5ea11ba8ed075d58a29485134e549be5
Daichi Kimura <daichi703n@gmail.com>
2019-09-15 21:35:01 +0900

    create scheduledTasks

---
+++ src/main/java/com/daichi703n/spotleft/ScheduledTasks.java
@@ -0,0 +1,48 @@
+package com.daichi703n.spotleft;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.daichi703n.spotleft.domain.AwsInstanceService;
+import com.daichi703n.spotleft.domain.SlackService;
+import com.daichi703n.spotleft.domain.SpotleftInfo;
+import com.github.seratch.jslack.api.webhook.WebhookResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTasks {
+
+  @Autowired
+  AwsInstanceService awsInstanceService;
+
+  @Autowired
+  SlackService slackService;
+
+  @Scheduled(cron = "${cron.cron1}")
+  public void notifyInstances() {
+    List<SpotleftInfo> instances = awsInstanceService.findAll();
+    List<SpotleftInfo> notifyInstances = new ArrayList<SpotleftInfo>();
+
+    instances.forEach(i -> {
+        if (
+            i.getRequireSpot()
+            && !i.getLifecycle().equals("spot")
+            && i.getState().equals("running")
+        ){
+            notifyInstances.add(i);
+        }
+    });
+    try {
+        WebhookResponse res = slackService.send(notifyInstances);
+        System.out.println(res);
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+  
+  }
+
+}
--- src/main/java/com/daichi703n/spotleft/SpotleftApplication.java
+++ src/main/java/com/daichi703n/spotleft/SpotleftApplication.java
@@ -2,8 +2,10 @@ package com.daichi703n.spotleft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpotleftApplication {
 
 	public static void main(String[] args) {
