2e2ea0704bd4a169c903a6a39e0a41419fc7d695
Daichi Kimura <daichi703n@gmail.com>
2019-09-16 22:27:02 +0900

    Add README on Home from GitHub

--- src/main/java/com/daichi703n/spotleft/web/SpotleftController.java
+++ src/main/java/com/daichi703n/spotleft/web/SpotleftController.java
@@ -4,17 +4,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
-@RequestMapping("/hello")
+@RequestMapping("/")
 public class SpotleftController {
 
     @RequestMapping(method = RequestMethod.GET)
-    // public String hello() {
-    public String hello(Model model) {
-        model.addAttribute("msg","サンプルメッセージ！");
+    public String index(Model model) {
+        String readmeUrl = "https://github.com/daichi703n/spotleft/blob/master/README.md";
+        RestTemplate restTemplate = new RestTemplate();
+        String resultStr = restTemplate.getForObject(readmeUrl, String.class);
+
+        String findStart = "<article ";
+        String findEnd = "</article>";
+        String readmeStr = resultStr
+            .substring(resultStr.indexOf(findStart), resultStr.indexOf(findEnd)+findEnd.length())
+            .replace("/daichi703n/spotleft","https://github.com/daichi703n/spotleft");
+
+        model.addAttribute("msg",readmeStr);
         return "index";
-        // return "hello/hello";
     }
 
 }
