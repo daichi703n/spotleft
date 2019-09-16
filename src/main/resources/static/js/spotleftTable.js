7f7703dc35e22bf149e7830d856eea0749710f9d
Daichi Kimura <daichi703n@gmail.com>
2019-09-17 01:05:18 +0900

    update DataTable config

--- src/main/resources/static/js/spotleftTable.js
+++ src/main/resources/static/js/spotleftTable.js
@@ -1,3 +1,8 @@
 jQuery(function($){
-    $("#spotleft-table").DataTable();
+    $("#spotleft-table").DataTable({
+        lengthChange: false,
+        searching: false,
+        displayLength: 50,
+        order: [ [ 1, "asc" ] ]
+    });
 });
