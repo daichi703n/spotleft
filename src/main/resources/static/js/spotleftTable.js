268ed116930171960ddad05e1cbe7f0876d3efb1
daichi-kimura <daichi.kimura@g.softbank.co.jp>
2019-10-01 10:29:57 +0900

    add searching on DataTable

--- src/main/resources/static/js/spotleftTable.js
+++ src/main/resources/static/js/spotleftTable.js
@@ -1,7 +1,7 @@
 jQuery(function($){
     $("#spotleft-table").DataTable({
         lengthChange: false,
-        searching: false,
+        // searching: false,
         displayLength: 50,
         order: [ [ 1, "asc" ] ]
     });
