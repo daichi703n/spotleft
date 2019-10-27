19c157d485a48e3e4e9032debb6900d0c320bca4
Daichi Kimura <daichi703n@gmail.com>
2019-10-27 21:10:03 +0900

    fix schema

--- src/main/resources/db/migration/V1_0_0__baseline.sql
+++ src/main/resources/db/migration/V1_0_0__baseline.sql
@@ -1,16 +1,10 @@
 CREATE TABLE instances (
   id VARCHAR(40) NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  deployment VARCHAR(255) DEFAULT '-',
+  name VARCHAR(255),
+  deployment VARCHAR(255),
   require_spot BOOLEAN,
   created_at TIMESTAMP DEFAULT NOW(),
   updated_at TIMESTAMP DEFAULT NOW() ON UPDATE NOW(),
 
   PRIMARY KEY(id)
 );
-
-INSERT INTO instances 
-  (id, name, deployment, require_spot)
-  VALUES
-  (1, 'test', 'test_dep', true)
-;
