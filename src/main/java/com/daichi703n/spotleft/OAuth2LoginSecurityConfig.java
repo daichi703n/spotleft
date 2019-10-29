119fe968136af6584a1c3f095d417599f5816fc6
Daichi Kimura <daichi703n@gmail.com>
2019-10-30 02:13:26 +0900

    Enable OAuth2

---
+++ src/main/java/com/daichi703n/spotleft/OAuth2LoginSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.daichi703n.spotleft;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class OAuth2LoginSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/","/css/**","js/**","/webjars/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            // .formLogin()
+            //     .loginPage("/login")
+            //     .permitAll()
+            //     .and()
+            .oauth2Login()
+            //     .loginPage("/login/oauth2")
+            //     .permitAll()
+                .and()
+            .logout()
+                .permitAll();
+    }
+}
--- src/main/java/com/daichi703n/spotleft/SpotleftApplication.java
+++ src/main/java/com/daichi703n/spotleft/SpotleftApplication.java
@@ -3,9 +3,11 @@ package com.daichi703n.spotleft;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableWebSecurity
 public class SpotleftApplication {
 
 	public static void main(String[] args) {
