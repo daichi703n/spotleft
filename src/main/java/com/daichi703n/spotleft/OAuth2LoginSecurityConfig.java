9a4322555dacddd82864dca73e0fb5003a973f57
Daichi Kimura <daichi703n@gmail.com>
2019-11-06 00:18:50 +0900

    Fix permit /js

--- src/main/java/com/daichi703n/spotleft/OAuth2LoginSecurityConfig.java
+++ src/main/java/com/daichi703n/spotleft/OAuth2LoginSecurityConfig.java
@@ -12,7 +12,7 @@ public class OAuth2LoginSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/","/css/**","js/**","/webjars/**").permitAll()
+                .antMatchers("/","/css/**","/js/**","/webjars/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
             // .formLogin()
